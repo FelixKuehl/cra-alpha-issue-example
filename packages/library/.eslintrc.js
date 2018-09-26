b62c3908d4ff7c22c5baca0ef86a7e7cd20eb002
Felix Kühl <felixk.kuehl@gmail.com>
2018-09-26 16:19:24 +0200

    initial commit

---
+++ packages/library/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true
+  },
+  extends: ['react-app'],
+  rules: {
+    'jsx-a11y/href-no-hash': [0]
+  }
+}
